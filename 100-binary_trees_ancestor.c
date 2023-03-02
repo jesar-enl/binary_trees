6ea34c6441b9e1af5f070bc7a3cabe52f1119e67
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-03-02 11:35:03 +0300

    LCA

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,23 +9,23 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
-	binary_tree_t *ancestor;
-	binary_tree_t *tmp1 = (binary_tree_t *)first;
-	binary_tree_t *tmp2 = (binary_tree_t *)second;
+	binary_tree_t *t1, *t2;
 
 	if (first == NULL || second == NULL)
 		return (NULL);
 
-	while (tmp1 != NULL)
+	if (first == second)
+		return ((binary_tree_t *)first);
+
+	t1 = first->parent;
+	t2 = second->parent;
+	if (t1 == NULL || first == t2 || (!t1->parent && t2))
 	{
-		ancestor = tmp2;
-		while (ancestor != NULL)
-		{
-			if (ancestor == tmp1)
-				return (ancestor);
-			ancestor = ancestor->parent;
-		}
-		tmp1 = tmp1->parent;
+		return (binary_trees_ancestor(first, t2));
 	}
-	return (NULL);
+	else if (t2 == NULL || t1 == second || (!t2->parent && t1))
+	{
+		return (binary_trees_ancestor(t1, second));
+	}
+	return (binary_trees_ancestor(t1, t2));
 }
